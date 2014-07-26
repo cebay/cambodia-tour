0df5ec20a089554b9f9a5e45002fd7d419a2d7af
radin-reth <radin.reth@gmail.com>
2014-07-26 11:47:38 +0700

    add responsive menu

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
                         </div>
                         <div class="col-xs-9">
                             <div class="row">
-                                <div class="col-xs-12">
+                                <div class="col-xs-12 hidden-xs">
                                     <form class="navbar-form navbar-right" role="search">
                                         <div class="form-group">
                                             <input type="text" class="form-control" placeholder="Search">
@@ -43,13 +43,25 @@
                                     </form>
                                 </div>
                             </div>
-                            <ul class="nav nav-top pull-right">
-                                <li class="active"><a href="#" class="hover-style">Home</a></li>
-                                <li><a href="#" class="hover-style">about us</a></li>
-                                <li><a href="#" class="hover-style">destination</a></li>
-                                <li><a href="#" class="hover-style">gallery</a></li>
-                                <li><a href="#" class="hover-style">contact us</a></li>
-                            </ul>
+                            <nav id="nav-custom" class="navbar navbar-default navbar-wrapper" role="navigation">
+                                <div class="navbar-header navbar-header-wrapper">
+                                    <button class="navbar-toggle navbar-toggle-wrapper" data-target=".navbar-ex-collapse" data-toggle="collapse" type="button">
+                                      <span class="sr-only">Toggle navigation</span>
+                                      <span class="icon-bar icon-bar-bg"></span>
+                                      <span class="icon-bar icon-bar-bg"></span>
+                                      <span class="icon-bar icon-bar-bg"></span>
+                                    </button>
+                                </div>
+                                <div class="collapse navbar-collapse navbar-ex-collapse navbar-collapse-wrapper">
+                                    <ul class="nav navbar-nav nav-top pull-right">
+                                        <li class="active"><a href="#" class="hover-style">Home</a></li>
+                                        <li><a href="#" class="hover-style">about us</a></li>
+                                        <li><a href="#" class="hover-style">destination</a></li>
+                                        <li><a href="#" class="hover-style">gallery</a></li>
+                                        <li><a href="#" class="hover-style">contact us</a></li>
+                                    </ul>
+                                </div>
+                            </nav>
                         </div>
                     </div>
                 </header>
