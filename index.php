1ca63d22b7f528bfd5e7629ed1aa43b6af81dec7
radin-reth <radin.reth@gmail.com>
2014-07-27 22:00:33 +0700

    add homepage section info

--- index.php
+++ index.php
@@ -160,6 +160,58 @@
                         </div>
                         <div class="col-xs-12 col-sm-9">
                             <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <div class="row info">
+                                        <div class="col-xs-9">
+                                            <h5 class="info-title">
+                                                Lorem ipsum dolor
+                                            </h5>
+                                            <p class="info-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                        </div>
+                                        <div class="col-xs-3 info-img">
+                                            <img src="images/island.jpg" alt="island.jpg" class="thumbnail">
+                                        </div>
+                                    </div>
+                                    <div class="row info">
+                                        <div class="col-xs-9">
+                                            <h5 class="info-title">
+                                                Lorem ipsum dolor
+                                            </h5>
+                                            <p class="info-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                        </div>
+                                        <div class="col-xs-3 info-img">
+                                            <img src="images/island.jpg" alt="island.jpg" class="thumbnail">
+                                        </div>
+                                    </div>
+                                    <div class="row info">
+                                        <div class="col-xs-9">
+                                            <h5 class="info-title">
+                                                Lorem ipsum dolor
+                                            </h5>
+                                            <p class="info-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                        </div>
+                                        <div class="col-xs-3 info-img">
+                                            <img src="images/island.jpg" alt="island.jpg" class="thumbnail">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
                                 <div class="row">
                                     <div class="col-xs-12 col-sm-6 col-md-4">
                                         <div class="panel-body">
