83c1f06c7bdde50f116528165cfb9168b6208618
radin-reth <radin.reth@gmail.com>
2014-08-02 22:24:56 +0700

    add contact us page

---
+++ contact-us/index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="../css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/main.css">
+        <script src="../js/vendor/modernizr-2.6.2.min.js"></script>
+
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="container-full-width">
+            <div class="container">
+                <header>
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <img src="../images/logo_mot.png">
+                        </div>
+                        <div class="col-xs-9">
+                            <div class="row">
+                                <div class="col-xs-12 hidden-xs">
+                                    <form class="navbar-form navbar-right" role="search">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" placeholder="Search">
+                                        </div>
+                                        <button type="submit" class="btn btn-default">
+                                            <span class="glyphicon glyphicon-search"></span> Submit
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                            <nav id="nav-custom" class="navbar navbar-default navbar-wrapper" role="navigation">
+                                <div class="navbar-header navbar-header-wrapper">
+                                    <button class="navbar-toggle navbar-toggle-wrapper" data-target=".navbar-ex-collapse" data-toggle="collapse" type="button">
+                                      <span class="sr-only">Toggle navigation</span>
+                                      <span class="icon-bar icon-bar-bg"></span>
+                                      <span class="icon-bar icon-bar-bg"></span>
+                                      <span class="icon-bar icon-bar-bg"></span>
+                                    </button>
+                                </div>
+                                <div class="collapse navbar-collapse navbar-ex-collapse navbar-collapse-wrapper">
+                                    <ul class="nav navbar-nav nav-top pull-right">
+                                        <li class="active"><a href="#" class="hover-style">Home</a></li>
+                                        <li><a href="#" class="hover-style">about us</a></li>
+                                        <li><a href="#" class="hover-style">destination</a></li>
+                                        <li><a href="#" class="hover-style">gallery</a></li>
+                                        <li><a href="#" class="hover-style">contact us</a></li>
+                                    </ul>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </header>
+                <div class="main-content">
+                    <div class="about-us">
+                        <h1 class="about-title">Contact Us</h1>
+                        <p class="about-short-desc">Lorem ipsum dolor sit amet</p>
+                        <p class="about-breadscrumb">home &raquo; contact-us</p>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6">
+                            <div class="panel panel-default">
+                                <div class="panel-heading"><h2>address</h2></div>
+                                <div class="panel-body">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua.</p>
+                                    <dl class="dl-horizontal">
+                                        <dt>Mobile phone:</dt>
+                                            <dd>093 555 225</dd>
+                                        <dt>Email</dt>
+                                            <dd>radin.reth@gmail.com</dd>
+                                        <dt>Facebook</dt>
+                                            <dd>http://www.faceboolk.com/radinreth</dd>
+                                        <dt>Twitter</dt>
+                                            <dd>@radin-reth</dd>
+                                    </dl> 
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6">
+                            <div class="panel panel-default">
+                                <div class="panel-heading"><h2>email us</h2></div>
+                                <div class="panel-body">
+                                    <form class="form-horizontal" role="form">
+                                        <div class="form-group">
+                                            <label for="inputName" class="col-lg-2 control-label">Name</label>
+                                            <div class="col-lg-10">
+                                                <input type="name" class="form-control" id="inputName" placeholder="Name">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputTelephone" class="col-lg-2 control-label">Telephone</label>
+                                            <div class="col-lg-10">
+                                                <input type="telephone" class="form-control" id="inputTelephone" placeholder="Telephone">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputEmail1" class="col-lg-2 control-label">*Email</label>
+                                            <div class="col-lg-10">
+                                                <input type="email" class="form-control" id="inputEmail1" placeholder="Email">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputMessage" class="col-lg-2 control-label">*Message</label>
+                                            <div class="col-lg-10">
+                                                <textarea rows="5" name="message" id="message" class="form-control"></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="col-lg-offset-2 col-lg-10">
+                                               <button type="submit" class="btn btn-default">Send</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="panel panel-default">
+                            <div class="panel-heading"><h2>Map</h2></div>
+                            <div class="panel-body">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <footer>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <ul class="footer-menu-lists">
+                                <li>contact us</li>
+                                <li>about us</li>
+                                <li>gallery</li>
+                                <li>destinations</li>
+                            </ul>
+                            <p class="copy-right">copyright &copy; 2014</p>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+
+        <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>-->
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            // (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            // function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            // e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            // e.src='//www.google-analytics.com/analytics.js';
+            // r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            // ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
