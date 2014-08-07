91d6a0c6b24687f626ba66d399f5f12bcfeb24de
radin-reth <radin.reth@gmail.com>
2014-08-07 23:32:53 +0700

    about us add

--- _admin/about-us.php
+++ _admin/about-us.php
@@ -59,7 +59,7 @@
                     </nav>
 
                     <!--main-->
-                    <div class="container">
+                    <div class="container content-wrapper">
                         <div class="row">
                           <!--left-->
                             <div class="col-md-3" id="leftCol">
@@ -74,119 +74,49 @@
                           
                           <!--right-->
                           <div class="col-md-9">
-                            <h2 id="sec0">Content</h2>
-                            <p>
-                              At Bootply we like to build simple Bootstrap templates that utilize the code Bootstap CSS without a lot of customization. Sure you can 
-                              find a lot of Bootstrap themes and inspiration, but these templates tend to be heavy on customization.</p>
-                            
-                            <hr>
-                            <p>
-                              Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
-                              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-                              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                              Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut.              
-                              Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
-                              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-                              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                              Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut!</p>
-                            
-                            <h2 id="sec1">Content</h2>
-                            <p>
-                              Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut.
-                            </p>
-                            <div class="row">
-                              <div class="col-md-6">
-                                <div class="panel panel-default">
-                                  <div class="panel-heading"><h3>Hello.</h3></div>
-                                  <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-                                    Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-                                    dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-                                    Aliquam in felis sit amet augue.
-                                  </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading title-bar">
+                                    <h4>logo</h4>
+                                    <a href="#" class="btn btn-default btn-xs edit-ico" data-toggle="tooltip" data-original-title="Edit" data-no-turbolink="true">
+                                      <span class="glyphicon glyphicon-pencil pull-right"></span>
+                                    </a>
                                 </div>
-                              </div>
-                              <div class="col-md-6">
-                                <div class="panel panel-default">
-                                  <div class="panel-heading"><h3>Hello.</h3></div>
-                                  <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-                                    Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-                                    dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-                                    Aliquam in felis sit amet augue.
-                                  </div>
+                                <div class="panel-body">
+                                    <div class="row">
+                                      <div class="col-xs-12 col-sm-4"></div>
+                                      <div class="col-xs-12 col-sm-8">
+                                         <div class="form-group">
+                                         <label for="inputEmail1" class="col-lg-2 control-label">Title</label>
+                                         <div class="col-lg-10">
+                                           <input type="email" class="form-control" id="inputEmail1" placeholder="Title">
+                                         </div>
+                                         </div>
+                                         <div class="form-group">
+                                         <label for="inputPassword1" class="col-lg-2 control-label">Description</label>
+                                         <div class="col-lg-10">
+                                           <textarea rows="5" name="message" id="message" class="form-control"></textarea>
+                                         </div>
+                                         </div>
+                                         <div class="form-group">
+                                         <div class="col-lg-offset-2 col-lg-10">
+                                           <div class="checkbox">
+                                             <label>
+                                               <input type="checkbox"> Remember me
+                                             </label>
+                                           </div>
+                                         </div>
+                                         </div>
+                                         <div class="form-group">
+                                         <div class="col-lg-offset-2 col-lg-10">
+                                           <button type="submit" class="btn btn-default">Sign in</button>
+                                         </div>
+                                         </div>
+                                        </form>
+                                      </div>
+                                    </div>
                                 </div>
-                              </div>  
                             </div>
-                            
-                            <hr>
-                            
-                            <h2 id="sec2">Section 2</h2>
-                            <p>
-                              Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
-                              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-                              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                              Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut!
-                            </p>
-                            <div class="row">
-                              <div class="col-md-4"><img src="//placehold.it/300x300" class="img-responsive"></div>
-                              <div class="col-md-4"><img src="//placehold.it/300x300" class="img-responsive"></div>
-                              <div class="col-md-4"><img src="//placehold.it/300x300" class="img-responsive"></div>
-                            </div>
-                            
-                            <hr>
-                            
-                            <h2 id="sec3">Section 3</h2>
-                            <p>
-                              Images are responsive sed @mdo but sum are more fun peratis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
-                              totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
-                              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-                              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                              Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut..</p>
-                            <p>
-                              Fos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-                              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                              Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut..</p>
-                            
-                            
-                            <h2 id="sec4">Section 4</h2>
-                            <p>
-                              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
-                              totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
-                              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-                              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                              Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut.</p>
-                              <p>
-                              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
-                              totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
-                              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-                              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                              Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut.</p>
-                              <p>
-                              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
-                              totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
-                              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-                              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                              Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut.</p>
-                              <p>
-                              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
-                              totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
-                              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-                              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                              Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut.</p>
-                            
-                            <hr>
-                            <h4><a href="http://www.bootply.com/Gygh9swISB">Edit on Bootply</a></h4>
-                            <hr>
-
-                            </div><!--/right-->
+                          </div>
                         </div><!--/row-->
                     </div><!--/container-->
                 </div>
