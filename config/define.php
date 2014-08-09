0b37a0e8518161643df707b38d0ca84254a99a56
radin-reth <radin.reth@gmail.com>
2014-08-09 21:44:11 +0700

    add config/define

---
+++ config/define.php
@@ -0,0 +1,13 @@
+<?php
+	define('HOST'			,'localhost');
+	define('USERNAME'		,'root');
+	define('PASSWORD'		,'');
+	define('DATABASE'		,'is440_new');
+
+	// TABLES
+	define('TBL_USER'		,'tbl_user');
+	define('TBL_ABOUT_US'	,'tbl_about_us');
+	define('TBL_TOUR'		,'tbl_tour');
+	define('TBL_CONTACT_US'	,'tbl_contact_us');
+	define('TBL_GALLERY'	,'tbl_gallery');
+?>
