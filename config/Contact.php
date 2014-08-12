43bd2d275431f67a5e5e95f48133437736f00159
radin-reth <radin.reth@gmail.com>
2014-08-12 22:56:42 +0700

    add lat(), lng(), update() method for Contact class

--- config/Contact.php
+++ config/Contact.php
@@ -12,6 +12,16 @@
 		function social ($social) {
 			return $social;
 		}
+		function lat ($lat) {
+			return $lat;
+		}
+		function lng ($lng) {
+			return $lng;
+		}
+		function user_id ($user_id) {
+			return $user_id;
+		}
+
 		function save() {
 
 			$description = $this->description;
@@ -22,7 +32,21 @@
 			$stmt = sprintf(CONTACT_SQL_INSERT, $description, $phone, $email, $social);
 			return mysql_query($stmt);
 		}
-		// basic info
+		function update() {
+			$lat 	 = $this->lat;
+			$lng 	 = $this->lng;
+			$user_id = $this->user_id;
+
+			$stmt 	 = sprintf(CONTACT_SQL_UPDATE, $lat, $lng, $user_id);
+			return mysql_query($stmt);
+		}
+		function upsert() {
+
+		}
+		function delete() {
+
+		}
+		// [x] basic info
 		// save map, logo
 		// update
 		// delete
